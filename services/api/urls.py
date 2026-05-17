5f5744a5c21060a8efc31f57257f319df4edbad6
baisal <krmzhnvbaisal@gmail.com>
2026-05-18 00:09:49 +0600

    fixed docker compose changes for mac

--- services/api/urls.py
+++ services/api/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path('api/admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/storage', include('storage.urls')),
 ]
