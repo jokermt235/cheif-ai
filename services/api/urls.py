1d5154a9f5c50f0444827f9d41d40bca7b979ba2
HP <tilekesenkulov06@gmail.com>
2026-05-22 14:18:43 +0600

    fixed swagger problem in urls.py

--- services/api/urls.py
+++ services/api/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     
     # Авторизация
     path('api/auth/', include('allauth.urls')),
-    
+
+    path('api/drf-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
     # Документация API
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
