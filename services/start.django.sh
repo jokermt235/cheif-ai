5096c7516127da081e3ff75590bbda59d229f047
HP <tilekesenkulov06@gmail.com>
2026-05-23 22:14:17 +0600

    Add automatic superuser creation on startup

--- services/start.django.sh
+++ services/start.django.sh
@@ -12,5 +12,17 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
+python manage.py shell -c "
+from storage.models import User; 
+import os;
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'test@gmail.com');
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '123451');
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password)
+    print('Superuser created successfully!')
+else:
+    print('Superuser already exists.')
+"
+
 export GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-" -b 0.0.0.0:8000 --timeout 30 --graceful-timeout 30 --forwarded-allow-ips=* --max-requests=10000 --chdir=/app"}
 gunicorn api.wsgi:application
