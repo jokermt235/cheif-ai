0518d8a5a5f48c655e51c419804f07e52876c449
HP <tilekesenkulov06@gmail.com>
2026-05-23 21:29:50 +0600

    urls.py fixed

--- services/api/urls.py
+++ services/api/urls.py
@@ -29,3 +29,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
