ad3c829a954599e7ea29c3b43901abac0c2acf61
HP <tilekesenkulov06@gmail.com>
2026-05-23 20:54:11 +0600

    fixing syntax errors

--- services/api/urls.py
+++ services/api/urls.py
@@ -1,3 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Настройка Swagger документации
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Chief AI API",
+      default_version='v1',
+      description="Документация для проекта Chief AI",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     # Админка
     path('api/admin/', admin.site.urls),
