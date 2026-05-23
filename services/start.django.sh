732d2d6623feda885048d794ea45a92ad2649bfb
aizirek7 <adokturbekk@gmail.com>
2026-05-24 00:15:55 +0600

    auto migrate and correct search page

--- services/start.django.sh
+++ services/start.django.sh
@@ -6,7 +6,7 @@ set -o nounset
 
 mkdir -p static
 python manage.py makemigrations
-#python manage.py migrate --run-syncdb
+python manage.py migrate
 python manage.py collectstatic --noinput
 
 export GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-" -b 0.0.0.0:8000 --timeout 30 --graceful-timeout 30 --forwarded-allow-ips=* --max-requests=10000 --chdir=/app"}
