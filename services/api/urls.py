dc01b353d2c8e4456210c57da3011c15700d71fc
HP <tilekesenkulov06@gmail.com>
2026-05-23 21:36:18 +0600

    urls.py fixed

--- services/api/urls.py
+++ services/api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Настройка Swagger документации
 schema_view = get_schema_view(
@@ -20,8 +22,10 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     
     path('api/storage/', include('storage.urls')),
-    path('api/', include('recipes.urls')),  
     
+    path('', include('recipes.urls')),  # <-- ИЗМЕНИЛИ ЗДЕСЬ (убрали api/)
+    
+    # Авторизация
     path('api/auth/', include('allauth.urls')),
     path('api/drf-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
@@ -29,8 +33,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
